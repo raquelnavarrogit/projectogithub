339b30271b17ca5f81afe0cc87653d50ab269b51
raquel <richo181095@gmail.com>
2024-02-25 11:00:51 +0100

    Añadido de atributos clase cliente

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,16 @@
 package projectogithub;
 
+import java.util.ArrayList;
+
 public class Cliente {
 
+	String nombre;
+	String apellido;
+	String dni;
+	ArrayList <Producto> compras = new ArrayList <Producto> ();
+	
+	
+	
+	
+	
 }

8d8b7939cb8ff83b6cdacf106901c6253765dfb8
raquel <richo181095@gmail.com>
2024-02-25 11:10:52 +0100

    Revert "Añadido de método añadir cliente"
    This reverts commit 2152aae89e9575d867430a5ee6ea1237eb624cd1.

--- Cliente.java
+++ Cliente.java
@@ -2,8 +2,6 @@ package projectogithub;
 
 import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
-
 public class Cliente {
 
 	String nombre;
@@ -12,22 +10,6 @@ public class Cliente {
 	ArrayList <Producto> compras = new ArrayList <Producto> ();
 	
 	
-	public Cliente(String nombre, String apellido, String dni) {
-		super();
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.dni = dni;
-	}
-	
-
-	public void agregarCliente () {
-		
-		String nom = JOptionPane.showInputDialog("Introduzca el nombre del cliente");
-		String ape = JOptionPane.showInputDialog("Introduzca el apellido del cliente");
-		String dni = JOptionPane.showInputDialog("Introduzca el dni del cliente");
-		
-		Cliente usuario = new Cliente (nom,ape,dni);
-	}
 	
 	
 	

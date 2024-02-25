40af8467600be504d55b08589398cc278854b430
raquel <richo181095@gmail.com>
2024-02-25 11:22:07 +0100

    Creación de constructor

--- Cliente.java
+++ Cliente.java
@@ -9,6 +9,18 @@ public class Cliente {
 	String dni;
 	ArrayList <Producto> compras = new ArrayList <Producto> ();
 	
+	public Cliente(String nombre, String apellido, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + "]";
+	}
+	
 	
 	
 	

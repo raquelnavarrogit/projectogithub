5c879b75e1a5bf6545cc132747661a76e7b00259
raquel <richo181095@gmail.com>
2024-02-26 12:59:43 +0100

    creación de otra Tienda

--- MainTienda.java
+++ MainTienda.java
@@ -4,10 +4,6 @@ public class MainTienda {
 
 	public static void main(String[] args) {
 		
-		Tienda teashop = new Tienda ();
-		
-		teashop.mostrarClientes();
-		teashop.mostrarProductos();
 		
 		
 		
@@ -21,7 +17,19 @@ public class MainTienda {
 		
 		
 		
-		System.out.println(teashop.toString());
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Tienda decathlon = new Tienda ();
+		System.out.println(decathlon.toString());
 	}
 
 }
